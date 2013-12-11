e5ccbcaad21402de9b58f2454c81ef4824dcbd5e
Denis Ovsienko <infrastation@yandex.ru>
2013-12-12 01:08:38 +0400

    add the 1st working revision of w703detect.sh

---
+++ w703detect.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TARGET=${1:-192.168.1.1}
+INDICES=localdata/indices
+
+echo "Target IPv4 address is $TARGET."
+while true; do
+	if ! ping -n -q -c2 -W2 -t1 $TARGET >/dev/null; then
+		echo 'Not yet...'
+		sleep 5
+		continue
+	fi
+	macaddr=`ip ne show $TARGET | cut -d' ' -f5`
+	echo "$TARGET on line with MAC address $macaddr"
+	if ! [ -s $INDICES ]; then
+		echo "The file $INDICES is missing or empty, cannot proceed"
+		continue
+	fi
+	index=`grep $macaddr $INDICES | cut -d' ' -f2`
+	if [ "$index" != "" ]; then
+		echo "Using index $index"
+		./w703config.sh $index $TARGET
+	fi
+done
