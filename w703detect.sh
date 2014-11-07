7be7fab993b2da9ebaf353fcb94283f984c545a7
Denis Ovsienko <denis@ovsienko.info>
2014-11-07 16:26:20 +0000

    do file check early

--- w703detect.sh
+++ w703detect.sh
@@ -3,6 +3,11 @@
 TARGET=${1:-192.168.1.1}
 INDICES=localdata/indices
 
+if ! [ -s $INDICES ]; then
+	echo "The file $INDICES is missing or empty, cannot proceed"
+	exit 1
+fi
+
 echo "Target IPv4 address is $TARGET."
 while true; do
 	if ! ping -n -q -c2 -W2 -t1 $TARGET >/dev/null; then
@@ -12,10 +17,6 @@ while true; do
 	fi
 	macaddr=`ip ne show $TARGET | cut -d' ' -f5`
 	echo "$TARGET on line with MAC address $macaddr"
-	if ! [ -s $INDICES ]; then
-		echo "The file $INDICES is missing or empty, cannot proceed"
-		continue
-	fi
 	index=`grep $macaddr $INDICES | cut -d' ' -f2`
 	if [ "$index" != "" ]; then
 		echo "Using index $index"
